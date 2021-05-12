8684bb72336681477679cd7a1deace5667649ad8
Dilshad B <dilshadmepcoece@gmail.com>
2021-05-12 18:50:04 +0530

    Test cases

--- src/test/java/app/TestApp.java
+++ src/test/java/app/TestApp.java
@@ -6,18 +6,7 @@ import org.junit.Test;
 
 import in.dilshad.service.BikeManager;
 
-public class PrimaryTest {
+public class TestApp {
 
-	/**
-	 * To display default bikes available in show room
-	 */
-	@Test
-	public void dilspayDefaultBikes() {
-		try {
-			BikeManager.displayBikes();
-		} catch (Exception e) {
-			System.out.println("Not displayed");
-		}
-	}
 
 }
--- src/test/java/in/dilshad/PrimaryTest.java
+++ src/test/java/in/dilshad/PrimaryTest.java
@@ -1,7 +1,5 @@
 package in.dilshad;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import in.dilshad.service.BikeManager;
